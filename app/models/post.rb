d91ea2f669108287dfc0e173ca3d0b662417f275
Shilcof <robert.shilcof@gmail.com>
2021-01-06 13:14:33 +0000

    commit tree 878d14b6afa4cd0fc6042a92f70dc5c1d2743c30

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,19 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
+  # accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
+
+  def tags_attributes=(tags_attributes)
+    if tags_attributes.try(:values)
+      tags_attributes.values.each do |tag_attributes|
+        if tag_attributes[:name].present?
+          tag = Tag.find_or_create_by(tag_attributes)
+          if !tags.include?(tag)
+            post_tags.build(tag: tag)
+          end
+        end      
+      end
+    end
+  end
 end
